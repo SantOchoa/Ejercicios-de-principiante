1c877575fbdef2467ad2e7348186d0e7c1a15ac3
Santiago Ochoa Paipilla <santiagop200611@gmail.com>
2025-06-17 21:28:39 -0500

    Termine mi mini proyecto de impar y par

--- numeros _impares/par_impar.py
+++ numeros _impares/par_impar.py
@@ -1,12 +1,22 @@
-numeros =  [1,2,3,4,5,6,7,8,9,10]
-
-
-def calnumimpar(numeros: list)->list: 
+def calnumpar(numeros: list)->int: 
     pares = 0
     for n in numeros:
         if n%2 ==0:
             pares += 1
     return pares
 
-countpar = calnumimpar(numeros)
-print(countpar)
+def calnumimpar(numeros:list)-> int:
+    impares =0 
+    for n in numeros:
+        if n%2 != 0 :
+             impares += 1 
+    return impares
+    
+
+
+
+numeros =  [1,2,3,4,5,6,7,8,9,10,11]
+countpar = calnumpar(numeros)
+countimpar = calnumimpar(numeros)
+print(f"Hay {countpar} numeros pares en la lista")
+print(f"Hay {countimpar} numeros inpares en la lista")
